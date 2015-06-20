26d62e8b90925ddd3abde0ad38edbc18260ccb8a
Kin Lane <kinlane@gmail.com>
2015-06-20 07:08:49 +0000

    changing home page to php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$hello = "Hello World!";
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Twitter - <?php echo $hello; ?></h1>
+
+<p>What do we want to do get people up to speed with using Twitter.</p>
+
+</body>
+</html>
